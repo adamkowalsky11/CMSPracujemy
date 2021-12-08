3b8422cc98ff7a0e41136ebe3d912053b5cc9afe
Julia Leszczynska <s17243@pjwstk.edu.pl>
2021-12-08 21:10:22 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,28 @@
 <template>
   <v-row justify="right" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div>
-        <h2>
-          <li v-for="(post, index) in allPosts" :key="index">
-            <nuxt-link :to="`/blog/${post.slug}`">{{post.title}}</nuxt-link>
-          </li>
-        </h2>
-      </div>
+  <v-carousel v-model="model">
+    <v-carousel-item
+      v-for="(color, i) in colors"
+      :key="color"
+    >
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
     </v-col>
   </v-row>
 </template>
