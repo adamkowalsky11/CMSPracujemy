e975b0630cb290a924f614e2a792b999efdc65d8
Julia Leszczynska <s17243@pjwstk.edu.pl>
2021-12-09 13:44:11 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,12 @@
 <template>
-  
-  
-  <v-row justify="center" align="center">
-    
-    <v-col cols="2" sm="8" md="6">
-      <h1 justify="center" align="center" class="my-15"> Znajdź swoją wymarzoną pracę </h1>
-      
-      
-  <v-carousel v-model="model">
-    <v-carousel-item
-    
-      v-for="(color) in colors"
-      :key="color"
-    >
-    
-      <v-sheet
-        :color="color"
-        height="100%"
-        tile
-        
-      >
-      
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-        <!--<div class="image"> <v-img max-height="128" max-width="250" src="https://www.datocms-assets.com/59409/1638966135-pjatk.jpeg"></v-img></div>-->
-          <div class="text-h2">
-            <!-- Comment SLAJD {{ i + 1 }}-->
-            Pracujemy.pl
-          </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
 
-    </v-col>
-  </v-row>
-  
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
 
 
@@ -56,16 +24,26 @@ export default defineComponent({
       }
     }`,
   },
-  data: () => ({
-      model: 0,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
-      ],
-    }),
+
+  data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+ 
 
 
     
